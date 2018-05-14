1d645f7cb7d758d6979d8b4096da1ac7e7c2e86d
uiqian <uiqian@163.com>
2018-05-14 17:16:37 +0800

    日常修改提交5.14

--- admindatap/admindata/views.py
+++ admindatap/admindata/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'index2.html')
+    return render(request,'admindata/index2.html')
